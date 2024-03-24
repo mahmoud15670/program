2ce87c74f549a5d4a61362ab002d64d04f47b5be
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-03-24 13:40:09 +0000

    Mar 24, 2024, 1:40 PM

--- mhlbia/homs/admin.py
+++ mhlbia/homs/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Patient)
-admin.site.register(Test)
-admin.site.register(Result)
+# admin.site.register(Patient)
+# admin.site.register(Test)
+# admin.site.register(Result)
--- mhlbia/homs/views.py
+++ mhlbia/homs/views.py
@@ -3,87 +3,87 @@ from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from .models import *
 
-def index(requset):
-    return render(requset, 'homs/layout.html')
+# def index(requset):
+#     return render(requset, 'homs/layout.html')
 
-class PatientView(generic.ListView):
-    model = Patient
-    template_name_suffix = '_list'
-    context_object_name = 'patients'
+# class PatientView(generic.ListView):
+#     model = Patient
+#     template_name_suffix = '_list'
+#     context_object_name = 'patients'
 
-class PatientDetilView(generic.DetailView):
-    model = Patient
-    template_name_suffix = '_detil'
-    context_object_name = 'patient'
+# class PatientDetilView(generic.DetailView):
+#     model = Patient
+#     template_name_suffix = '_detil'
+#     context_object_name = 'patient'
 
-class PatientCreateView(generic.CreateView):
-    model = Patient
-    fields = '__all__'
-    template_name_suffix = '_create'
-    success_url = reverse_lazy('patient')
-    def form_valid(self, form):
-        object = form.save()
-        for result in object.result_set.all():
-            if object.gender == 'male':
-                result.ref = result.test.ref_male
-                result.save()
-            else:
-                result.ref = result.test.ref_female
-                result.save()
-        return super().form_valid(form)
+# class PatientCreateView(generic.CreateView):
+#     model = Patient
+#     fields = '__all__'
+#     template_name_suffix = '_create'
+#     success_url = reverse_lazy('patient')
+#     def form_valid(self, form):
+#         object = form.save()
+#         for result in object.result_set.all():
+#             if object.gender == 'male':
+#                 result.ref = result.test.ref_male
+#                 result.save()
+#             else:
+#                 result.ref = result.test.ref_female
+#                 result.save()
+#         return super().form_valid(form)
     
 
-class PatientEditView(generic.UpdateView):
-    model = Patient
-    fields = '__all__'
-    template_name_suffix = '_edit'
-    success_url = reverse_lazy('patient')
-    def form_valid(self, form):
-        object = form.save()
-        for result in object.result_set.all():
-            if object.gender == 'male':
-                result.ref = result.test.ref_male
-                result.save()
-            else:
-                result.ref = result.test.ref_female
-                result.save()
-        return super().form_valid(form)
-class PatientDeleteView(generic.DeleteView):
-    model = Patient
-    template_name_suffix = '_delete'
-    success_url = reverse_lazy('patient')
+# class PatientEditView(generic.UpdateView):
+#     model = Patient
+#     fields = '__all__'
+#     template_name_suffix = '_edit'
+#     success_url = reverse_lazy('patient')
+#     def form_valid(self, form):
+#         object = form.save()
+#         for result in object.result_set.all():
+#             if object.gender == 'male':
+#                 result.ref = result.test.ref_male
+#                 result.save()
+#             else:
+#                 result.ref = result.test.ref_female
+#                 result.save()
+#         return super().form_valid(form)
+# class PatientDeleteView(generic.DeleteView):
+#     model = Patient
+#     template_name_suffix = '_delete'
+#     success_url = reverse_lazy('patient')
 
-class TestView(generic.ListView):
-    model = Test
-    context_object_name = 'tests'
+# class TestView(generic.ListView):
+#     model = Test
+#     context_object_name = 'tests'
 
-class TestDetilView(generic.DetailView):
-    model = Test
-    template_name_suffix = '_detil'
-    context_object_name = 'test'
+# class TestDetilView(generic.DetailView):
+#     model = Test
+#     template_name_suffix = '_detil'
+#     context_object_name = 'test'
 
-class TestCreateView(generic.CreateView):
-    model = Test
-    fields = '__all__'
-    template_name_suffix = '_create'
-    success_url = reverse_lazy('test')
+# class TestCreateView(generic.CreateView):
+#     model = Test
+#     fields = '__all__'
+#     template_name_suffix = '_create'
+#     success_url = reverse_lazy('test')
 
-class TestEditView(generic.UpdateView):
-    model = Test
-    fields = '__all__'
-    template_name_suffix = '_edit'
-    success_url = reverse_lazy('test')
-class TestDeleteView(generic.DeleteView):
-    model = Test
-    template_name_suffix = '_delete'
-    success_url = reverse_lazy('test')
+# class TestEditView(generic.UpdateView):
+#     model = Test
+#     fields = '__all__'
+#     template_name_suffix = '_edit'
+#     success_url = reverse_lazy('test')
+# class TestDeleteView(generic.DeleteView):
+#     model = Test
+#     template_name_suffix = '_delete'
+#     success_url = reverse_lazy('test')
 
-class ResultCreateView(generic.UpdateView):
-    model = Result
-    fields = ['result', 'ref']
-    template_name_suffix = '_create'
-    success_url = reverse_lazy('patient')
+# class ResultCreateView(generic.UpdateView):
+#     model = Result
+#     fields = ['result', 'ref']
+#     template_name_suffix = '_create'
+#     success_url = reverse_lazy('patient')
     
 
-class ResultDetilView(generic.DetailView):
-    model = Result
+# class ResultDetilView(generic.DetailView):
+#     model = Result
