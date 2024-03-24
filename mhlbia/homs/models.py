09758c80d5afa4c75d3b31183de923d6d9403251
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-03-24 14:28:40 +0000

    Mar 24, 2024, 2:28 PM

--- mhlbia/homs/models.py
+++ mhlbia/homs/models.py
@@ -5,7 +5,7 @@ import random
 
 
 class Patient(models.Model):
-    patient_id = models.CharField(max_length=12, unique=True, default=random.randint())
+    patient_id = models.CharField(max_length=12, unique=True)
     name = models.CharField(max_length=60, name='name')
     age = models.PositiveSmallIntegerField(name='age',)
     date = models.CharField(max_length=5, choices=[('year', 'year'), ('month', 'month'), ('day', 'day')])
@@ -17,6 +17,8 @@ class Patient(models.Model):
         
     def generate_rondom(self):
         num = ''.join(str(random.randint(0,9)) for _ in range(12))
+        self.patient_id = num
+        return self.patient_id
     
     def total_prise(self):
         return sum([query.prise for query in self.test.all()])
