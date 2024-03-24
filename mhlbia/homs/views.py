dbcd6ae1e22a2e3abcf4c794e1a03b8d02d598fc
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-03-24 10:07:07 +0000

    Mar 24, 2024, 10:07 AM

--- mhlbia/homs/views.py
+++ mhlbia/homs/views.py
@@ -21,13 +21,10 @@ class PatientCreateView(generic.CreateView):
     fields = '__all__'
     template_name_suffix = '_create'
     success_url = reverse_lazy('patient')
-    def post(self, request, *args, **kwargs):
-        def form_valid(self, form):
-            form
-            print(self)
-            return super().form_valid(form)
-        
-        return super().post(request, *args, **kwargs)
+    def form_valid(self, form):
+        form
+        print(self.object)
+        return super().form_valid(form)
 
 class PatientEditView(generic.UpdateView):
     model = Patient
