17fafd31822d7ebf1b0ee79ea7de24f45dd68acd
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-03-25 20:50:24 +0000

    Mon, Mar 25, 2024, 10:50 PM +02:00

--- mhlbia/mhlbia/settings.py
+++ mhlbia/mhlbia/settings.py
@@ -33,6 +33,7 @@ CSRF_TRUSTED_ORIGINS = ['https://symmetrical-space-rotary-phone-56v4j5gqpqjf4vxp
 
 INSTALLED_APPS = [
     'homs',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
