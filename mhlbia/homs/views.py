e3e56cc47cf107322c120440c5df6b5e73c77981
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-03-29 19:01:02 +0000

    Fri, Mar 29, 2024, 9:01 PM +02:00

--- mhlbia/homs/views.py
+++ mhlbia/homs/views.py
@@ -22,7 +22,7 @@ class PatientCreateView(generic.CreateView):
     template_name_suffix = '_create'
     success_url = reverse_lazy('patient')
     def form_valid(self, form):
-        form.
+        form.da
         # for result in object.result_set.all():
         #     if object.gender == 'male':
         #         result.ref = result.test.ref_male
