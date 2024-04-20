99be2e5f254ae4d00c234be7376a09c3e2546c7c
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-04-20 07:42:37 +0000

    Apr 20, 2024, 7:42 AM

--- namer/lib/main.dart
+++ namer/lib/main.dart
@@ -37,7 +37,7 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          Card.outlined()
+          Card.outlined(child: Text('hi'),)
         ],
       ),
     );
