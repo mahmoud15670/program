420dde1ecb500aca15024ee531a355262f1bb42d
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-04-26 13:23:41 +0000

    Fri, Apr 26, 2024, 4:23 PM +03:00

--- tester/lib/main.dart
+++ tester/lib/main.dart
@@ -66,7 +66,7 @@ class _MyHomePageState extends State<MyHomePage> {
               style: Theme.of(context).textTheme.displayLarge,
             ),
             Padding(
-              padding: const EdgeInsets.all(100),
+              padding: const EdgeInsets.all(100.0),
               child: Card(
                 color: Theme.of(context).colorScheme.inversePrimary,
                 child: Text(
